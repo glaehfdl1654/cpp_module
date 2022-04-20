307ff76c85ffbb6ac4b05113ea5c543c33fc0ed9
jaejeong <glaehfdl1654@naver.com>
2022-04-20 17:22:36 +0900

    cpp00 ex00 finish!

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -3,35 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaejeong <jaejeong@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jaejeong <jaejeong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 15:09:47 by jaejeong          #+#    #+#             */
-/*   Updated: 2022/02/04 15:09:48 by jaejeong         ###   ########.fr       */
+/*   Updated: 2022/04/20 17:18:58 by jaejeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string>
 
-int	main(int argc, char *argv[])
+using std::cout;
+using std::endl;
+
+int	main(int argc, const char *argv[])
 {
 	std::string	str;
 
 	if (argc == 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
 		return (0);
 	}
 	for (int i = 1; i < argc; i++)
 	{
 		for (int j = 0; argv[i][j]; j++)
-		{
-			if (islower(argv[i][j]))
-				str.push_back(toupper(argv[i][j]));
-			else
-				str.push_back(argv[i][j]);
-		}
+			str.push_back((char)toupper(argv[i][j]));
 	}
-	std::cout << str << std::endl;
+	cout << str << endl;
 	return (0);
 }
