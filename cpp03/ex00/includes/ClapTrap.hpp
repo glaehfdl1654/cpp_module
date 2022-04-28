f3b00baa94fdff70934a387acfcc9518d6e40df2
Jaemin Jeong <jaejeong@c5r8s1.42seoul.kr>
2022-04-28 20:50:42 +0900

    [DONE] : cpp03 ex00

--- cpp03/ex00/includes/ClapTrap.hpp
+++ cpp03/ex00/includes/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: jaejeong <jaejeong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 03:11:02 by jaejeong          #+#    #+#             */
-/*   Updated: 2022/04/26 03:14:14 by jaejeong         ###   ########.fr       */
+/*   Updated: 2022/04/28 20:35:30 by jaejeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,28 @@
 class ClapTrap
 {
 private: 
+	std::string	name;
 	int	hitPoints;
 	int	energePoints;
 	int	attackDamage;
 public:
 	ClapTrap();
+	ClapTrap(const std::string &name);
 	ClapTrap(const ClapTrap& other);
 	ClapTrap& operator=(const ClapTrap& rhs);
 	~ClapTrap();
 	void	attack(const std::string &target);
 	void	takeDamage(unsigned int amount);
 	void	beRepaired(unsigned int amount);
+
+	std::string	getName();
+	int		getHitPoints();
+	int		getEnergePoints();
+	int		getAttackDamage();
+	void	setName(std::string name);
+	void	setHitPoints(int hitPoints);
+	void	setEnergePoints(int energePoints);
+	void	setAttackDamage(int attackDamage);
 };
 
 #endif
